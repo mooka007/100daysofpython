4b7559d48e8032900de69d1d940fa0ca70d68500
Mooka <47282413+mooka007@users.noreply.github.com>
2023-08-04 02:56:48 +0100

    adding lives option

--- hangman.py
+++ hangman.py
@@ -1,8 +1,64 @@
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
 words = ["btata", "maticha", "gar3a", "falfla"]
 chosen_word = random.choice(words).lower()
-
+lives = 6
 # blank list to check if its the right letter or not
 display = []
 for letter in range(len(chosen_word)):
@@ -16,9 +72,19 @@ while not theEnd:
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
-    print(display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            theEnd = True
+            print('Game Over')
+
+    print(f"{' '.join(display)}")
+
 
     if "-" not in display:
         theEnd = True
-        print("You win! Congratulation :)")
+        print(" Congratulation! You win :)")
     
+    print(stages[lives])
+# ghadi nzid whd function kat randomi words by inputf
