d8193048a63e4acd817492b0efb4302447f7be43
Mooka <47282413+mooka007@users.noreply.github.com>
2023-08-05 19:35:35 +0100

    prime checker

--- test.py
+++ test.py
@@ -50,3 +50,20 @@
 # coverRage = 5
 # paint_calc(height=test_h, width=test_w, cover=coverRage)
 
+# *************************** prime checker ****************************** #
+
+# def prime_checker(number):
+#     is_prime = True
+#     for i in range(2, number):
+#         if number % i == 0:
+#             is_prime == False
+#     if is_prime:
+#         print("its a prime number.")
+#     else:
+#         print("its not a prime number")
+
+# n = int(input("Check this Number : "))
+# prime_checker(number=n)
+
+
+
