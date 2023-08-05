a90cb5ba885df8d098901694d199d5f3090f4148
Mooka <47282413+mooka007@users.noreply.github.com>
2023-08-05 19:05:25 +0100

    paint area calc

--- test.py
+++ test.py
@@ -35,3 +35,18 @@
 #     else:
 #         print(number)
 
+# *************************** paint area ****************************** #
+
+# import math
+
+# def paint_calc(height, width, cover):
+#     area = height * width
+#     numofcans = math.ceil(area / cover)
+#     print(f"you will need {numofcans} cans of painting")
+
+
+# test_h = int(input("Height of wall : "))
+# test_w = int(input("Width of wall : "))
+# coverRage = 5
+# paint_calc(height=test_h, width=test_w, cover=coverRage)
+
