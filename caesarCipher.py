d44be8a66e915cf798614cc2008ef0e1b1f05d99
Mooka <47282413+mooka007@users.noreply.github.com>
2023-08-11 01:37:44 +0100

    encrypt

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,6 +1,20 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(plain_text, shift_amount):
+    ciphered_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        ciphered_text += new_letter
+    print(f"the encoded text is {ciphered_text}")
+
+
+
+encrypt(plain_text=text, shift_amount=shift)
