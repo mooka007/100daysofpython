59fe1b852bcfd12eeaed3300b2b350d7f4bdcde7
Mooka <mooka3084@gmail.com>
2023-08-21 02:46:37 +0100

    edit

--- calculator.py
+++ calculator.py
@@ -55,89 +55,12 @@ while True:
         equation = f"{num1} {pickedSymbol} {num2}"
         print(f"{equation} = {answer}")
         history[equation] = answer
-        pass
     else:
         print("Invalid operation symbol.")
         break
 
 print("History of calculations:")
-for equation, result in history.items():
-    print(f"{equation} = {result}")
+for equation, answer in history.items():
+    print(f"{equation} = {answer}")
 
 
-
-
-# //////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def ad(n1, n2):
-#     return n1 + n2
-
-# def sub(n1, n2):
-#     return n1 - n2
-
-# def mult(n1, n2):
-#     return n1 * n2
-
-# def div(n1, n2):
-#     return n1 / n2
-
-# op = {
-#     "+": ad,
-#     "-": sub,
-#     "*": mult,
-#     "/": div
-# }
-
-# history = {}
-
-# while True:
-#     num1 = int(input("What is the first number? (Enter 0 to exit): "))
-#     if num1 == 0:
-#         break
-
-#     while True:
-#         pickedSymbol = input("Enter the operation symbol: ")
-#         if pickedSymbol not in op:
-#             print("Invalid operation symbol. Please try again.")
-#         else:
-#             break
-
-#     num2 = int(input("What is the second number? : "))
-#     calculation_func = op[pickedSymbol]
-#     answer = calculation_func(num1, num2)
-#     equation = f"{num1} {pickedSymbol} {num2}"
-
-#     print(f"{equation} = {answer}")
-
-#     history[equation] = answer
-
-# print("History of calculations:")
-# for equation, result in history.items():
-#     print(f"{equation} = {result}")
