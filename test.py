40de3657eac4b7f16cb99bc1a056150a7db32707
Mooka <mooka3084@gmail.com>
2023-10-25 23:01:37 +0100

    Object

--- test.py
+++ test.py
@@ -91,3 +91,38 @@
 #     student_grades[student] = "Fail"
     
 # print(student_grades)
+
+
+# ***************************  Class user ****************************** #
+
+# class User:
+    
+#     def __init__(self, user_id, username):
+#         self.id = user_id
+#         self.username = username
+#         self.followers = 0
+#         self.following = 0
+    
+#     def follow(self, user):
+#         user.followers += 1
+#         self.following += 1
+
+# user_1 = User("001", "angela")
+# user_2 = User("002", "karma")
+
+# user_1.follow(user_2)
+# user_2.follow(user_1)
+# print(user_1.followers)
+# print(user_1.following)
+
+
+
+
+
+
+
+
+
+
+
+
