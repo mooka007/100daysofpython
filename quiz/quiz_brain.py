907dc85fdf880a19771b2f42bbfd6420a4174212
Mooka <mooka3084@gmail.com>
2023-10-26 18:23:02 +0100

    quizz

---
+++ quiz/quiz_brain.py
@@ -0,0 +1,15 @@
+# TOdo : asking the question
+#        checking if the answer is correct 
+#        checking if we're the end  of the quiz
+
+# Create a class called QuizBrain
+# Write an __init__() method
+# initialise the question_number to 0
+# initialise the question_list to an input 
+
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+        
+# 007 quiz 04:58
