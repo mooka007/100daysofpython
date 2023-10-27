ee32737604186c06a74e67d53415d91e430e10ff
Mooka <mooka3084@gmail.com>
2023-10-27 18:42:03 +0100

    asking people for answer and display question

--- quiz/quiz_brain.py
+++ quiz/quiz_brain.py
@@ -12,4 +12,8 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = q_list
         
-# 007 quiz 04:58
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        input(f"Q.{self.question_number}: {current_question.text} (True / False) : ")     
