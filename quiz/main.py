aa1a44a5f44b9fc356de627959072ddad9b1730f
Mooka <mooka3084@gmail.com>
2023-10-27 20:37:59 +0100

    done

--- quiz/main.py
+++ quiz/main.py
@@ -13,3 +13,9 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_question():
     quiz.next_question()
+    
+
+print("You've completed the Quiz")
+print(f"Your Final Score was : {quiz.score}/{quiz.question_number}")
+
+# opentdb.com/api_config.php
