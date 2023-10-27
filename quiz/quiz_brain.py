13af51dc21fd65f74c105943e3e3b5fc0b02ad2c
Mooka <mooka3084@gmail.com>
2023-10-27 19:06:00 +0100

    next question

--- quiz/quiz_brain.py
+++ quiz/quiz_brain.py
@@ -16,4 +16,7 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True / False) : ")     
+        input(f"Q.{self.question_number}: {current_question.text} (True / False) : ")
+    
+    def still_has_question(self):
+        return self.question_number < len(self.question_list)
