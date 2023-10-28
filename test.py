b782a6dc66019a796e927b1bcbcf3d43ae234971
Mooka <mooka3084@gmail.com>
2023-10-28 17:41:53 +0100

    draw Spirograph

--- test.py
+++ test.py
@@ -129,3 +129,28 @@
 
 # *************************** draw Spirograph ****************************** #
 
+# import turtle as t
+# import random
+
+# tim = t.Turtle()
+# t.colormode(255)
+
+
+# def random_color():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     color = (r, g, b)
+#     return color
+
+# tim.speed("fastest")
+# def draw_spiro(sizeofGap):
+#     for _ in range(int(360 / sizeofGap)):
+#         tim.color(random_color())
+#         tim.circle(100)
+#         tim.setheading(tim.heading() + sizeofGap)
+
+# draw_spiro(5)   
+
+# screen = t.Screen()
+# screen.exitonclick()
