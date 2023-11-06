476994a59330f3753a0c838f793d0e0479fb0ba6
Mooka <mooka3084@gmail.com>
2023-11-06 20:22:14 +0100

    testing onkey

--- test.py
+++ test.py
@@ -176,13 +176,18 @@ def turn_left():
 def turn_right():
     tim.right(10)
 
-
-
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+    
 screen.listen()
-screen.onkey(move_forward, 'w')
-screen.onkey(move_backward, 's')
-screen.onkey(turn_left, 'a')
-screen.onkey(turn_right, 'd')
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(clear, "c")
 screen.exitonclick()
 
 
