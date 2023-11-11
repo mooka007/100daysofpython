abf6bd493d2b3e68183e2339c2b47d2dd12490d4
Mooka <mooka3084@gmail.com>
2023-11-11 01:28:36 +0100

    test turtle race

--- test.py
+++ test.py
@@ -158,41 +158,79 @@
 # *************************** turn right & left turtle ****************************** #
 
 
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
+# tim = Turtle()
+# screen = Screen()
+
+# def move_forward():
+#     tim.forward(10)
+
+# def  move_backward():
+#     tim.backward(10)
+
+# def turn_left():
+#     new_heading = tim.heading()  + 10
+#     tim.setheading(new_heading)
+
+# def turn_right():
+#     tim.right(10)
+
+# def clear():
+#     tim.clear()
+#     tim.penup()
+#     tim.home()
+#     tim.pendown()
+    
+# screen.listen()
+# screen.onkey(move_forward, "w")
+# screen.onkey(move_backward, "s")
+# screen.onkey(turn_left, "a")
+# screen.onkey(turn_right, "d")
+# screen.onkey(clear, "c")
+# screen.exitonclick()
+
+
+# *************************** Turtle Race ****************************** #
+
+from turtle import Turtle, Screen
+import random
+
+race_on = False
 tim = Turtle()
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make Your Bet", prompt="Which Turtle will win the Race! Pick a color : ")
+colors = ["red", "orange", "green", "blue", "purple"]
+y_position = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
-def move_forward():
-    tim.forward(10)
-
-def  move_backward():
-    tim.backward(10)
-
-def turn_left():
-    new_heading = tim.heading()  + 10
-    tim.setheading(new_heading)
-
-def turn_right():
-    tim.right(10)
-
-def clear():
-    tim.clear()
-    tim.penup()
-    tim.home()
-    tim.pendown()
+for turtle_index in range(0, 5):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-240, y=y_position[turtle_index])
+    all_turtles.append(new_turtle)
     
-screen.listen()
-screen.onkey(move_forward, "w")
-screen.onkey(move_backward, "s")
-screen.onkey(turn_left, "a")
-screen.onkey(turn_right, "d")
-screen.onkey(clear, "c")
+if user_bet:
+    race_on = True
+
+while race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} Turlte is the winner!")
+            else:
+                print("Sorry you lost! Better luck next time.")
+                
+        speed = random.randint(1, 10)
+        turtle.forward(speed)    
+
 screen.exitonclick()
 
-
-
-
+# DAy19 006 7:44
 
 
 
